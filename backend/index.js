d6222942d6edbad8acb60fa2721fbff302e28685
ravisuneel6565 <ravisuneel6262@gmail.com>
2024-11-15 16:59:49 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.send("project api is runnig")
+})
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1/', projectRoute);
